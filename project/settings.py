b6af23a42457a5e662dc194c73a3db7435b158fe
karim-aboelazm <karimaboelazm28@gmail.com>
2023-02-25 23:47:51 +0200

    dbHost

--- project/settings.py
+++ project/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
        'NAME'    : env('DATABASE_NAME'),
        'USER'    : env('DATABASE_USER'),
        'PASSWORD': env('DATABASE_PASS'), 
-       'HOST'    : env('DATABASE_HOST') ,      
+       'HOST'    :'' ,      
    }
 }
 
