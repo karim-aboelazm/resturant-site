1f27afe5ef9f72bc106fd8e54b2ea1b4052dda02
KARIM ABOELAZM <74789405+karim-aboelazm@users.noreply.github.com>
2023-03-09 14:55:26 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1','baba-voss.herokuapp.com','www.babavoss.site','babavoss.site']
 
 
 # Application definition
