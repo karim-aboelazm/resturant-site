23a6b63280aaeea11a6f273c68bddfa5bd32e366
KARIM ABOELAZM <74789405+karim-aboelazm@users.noreply.github.com>
2023-03-09 14:39:20 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -153,5 +153,8 @@ MEDIAFILES_DIRS = [os.path.join(BASE_DIR,'mediafiles')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
+if os.getcwd() == '/resturant':
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    DEBUG = False
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
